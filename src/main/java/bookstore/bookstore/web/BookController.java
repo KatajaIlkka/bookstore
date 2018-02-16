644d37d1f3f258ed079fcbbdd320d477bc6a0924
Katajamäki Ilkka <a1700312@myy.haaga-helia.fi>
2018-02-16 11:22:36 +0200

    REST

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -1,11 +1,14 @@
 package bookstore.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import bookstore.bookstore.domain.Book;
 import bookstore.bookstore.domain.BookRepository;
@@ -23,6 +26,18 @@ public class BookController {
 	public String bookList(Model model){
 		model.addAttribute("books", brepository.findAll());
 		return "booklist";
+	}
+	
+	// REST get all books
+	@RequestMapping(value="/books", method=RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) brepository.findAll();
+	}
+	
+	//REST books by id
+	@RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId){
+		return brepository.findOne(bookId);
 	}
 
 	@RequestMapping(value="/add")
